69e939902cc9b5426055a87e7e18acdbb434bc49
Mario Ramirez Jimenez <73549015+MarioRamJim@users.noreply.github.com>
2022-11-23 10:00:31 +0100

    Contenedor del laberinto creado
    Contenedor del laberinto creado como un widget en el constructor de la pantalla en la cual se colocará

--- MainWindow_GUI.py
+++ MainWindow_GUI.py
@@ -1,27 +1,28 @@
 from PySide6.QtCore import (QMetaObject, QRect)
 from PySide6.QtGui import (QFont)
-from PySide6.QtWidgets import ( QLabel, QMenuBar, QPushButton, QStatusBar, QWidget, QLineEdit)
+from PySide6.QtWidgets import ( QLabel, QMenuBar, QPushButton, QStatusBar, QWidget, QLineEdit,QGridLayout,QMainWindow)
 from laberintoContainer import laberintoContainer
 
 # Definición de la forma que tendrá la pantalla de login e inicialización de sus componentes
 
 class MainWindow(object):
     def setupUi(self, MainWindow):
+
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
-
-        MainWindow.resize(1280, 720)
+        
+        MainWindow.resize(1080,720)
 
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.laberinto = laberintoContainer(5)
-        self.laberinto.setObjectName("laberinto")
-        self.laberinto.setGeometry(100,100,100,100)
-        self.laberinto.width = 1280
-        self.laberinto.width = 720
 
-        self.laberinto.setParent(MainWindow)
+        self.laberinto = QWidget()
+        self.laberinto.setStyleSheet("background-color:black")
+        self.laberinto.setParent(self.centralwidget)
+        self.laberinto.setObjectName(u"laberinto")
+        self.laberinto.setGeometry(QRect(150, 170, 150, 150))
 
         self.retranslateLogin(MainWindow)
 
