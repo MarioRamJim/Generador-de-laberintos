6b5c10c9778f5d5b08c00857f84ce9c9baf2b2bf
Mario Ramirez Jimenez <73549015+MarioRamJim@users.noreply.github.com>
2022-12-02 11:39:36 +0100

    Actualización en los imports

--- Main.py
+++ Main.py
@@ -4,8 +4,10 @@ from PySide6.QtSql import *
 from PySide6.QtCore import *
 from Mainwindow import MainWindow
 from PySide6.QtCore import QUrl
-
+from celdaLaberinto import celdaLaberinto
+from SecondWindow import SecondWindow
 # Inicialización de la pantalla principal y ejecución del programa
+
 
 if __name__ == '__main__':
 
